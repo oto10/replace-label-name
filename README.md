510ec0e892769e91036410532cd00fcc24ca51b4
oto10 <ohashi11106@gmail.com>
2022-09-18 12:54:03 +0900

    added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# replace-label-name
-labelmeで作成したCOCO形式のアノテーションファイルのラベル名を変更します。
+## replace-label-name
+
+labelmeで作成したCOCO形式のアノテーションファイルにおける特定のラベル名を変更します。
+
+## 使い方
+
+- COCO形式のアノテーションファイルが格納されたフォルダパスを設定してください。
+- 変更前のラベル名（pre-label-name）と変更後のラベル名（post-label-name）を指定して以下のコードを実行すると、先程指定したフォルダに格納されたアノテーションファイルのラベル名が「pre-label-name」から「post-label-name」になります。
+
+```
+$ python replace_label.py pre-label-name post-label-name
+```
---
+++ replace_label.py
@@ -0,0 +1,22 @@
+import os
+import glob
+import sys
+from pathlib import Path
+
+def replace_label_name():
+    args = sys.argv
+    if len(args) != 3:
+        print('以下の形式で変更前のラベル名と変更後のラベル名を指定してください。')
+        print('$ replace_label.py pre-label-name post-label-name')
+        sys.exit(1)
+
+    files = glob.glob('/path/to/folder/*.json') # パスの設定
+    for file in files:
+        read_path = Path(file)
+        content = read_path.read_text()
+        pre_label_name = f'\"label\": \"{args[1]}\"'
+        post_label_name = f'\"label\": \"{args[2]}\"'
+        content = content.replace(pre_label_name, post_label_name)
+        read_path.write_text(content)
+
+replace_label_name()
